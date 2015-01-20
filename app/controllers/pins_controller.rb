e828c4d6056ed373af08b59c0b16919d49fbc37a
Carly DaCosta <carly.a.dacosta@gmail.com>
2015-01-20 15:25:58 -0800

    Added user/pin association. Authenticated users.

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,5 +1,10 @@
 class PinsController < ApplicationController
+  # before action is saying go to the set_pin function first, then the show, edit, update, and destroy.
+  # so @pin in the set_pin function at the bottom is accessed first before the other functions. 
   before_action :set_pin, only: [:show, :edit, :update, :destroy]
+  before_action :correct_user, only: [:edit, :destroy]
+  # makes user sign in when trying to visit any page except the index or show pages
+  before_action :authenticate_user!, except: [:index, :show]
 
   respond_to :html
 # Read/show all the pins
@@ -13,8 +18,7 @@ class PinsController < ApplicationController
   end
 # Create new pin
   def new
-    @pin = Pin.new
-    respond_with(@pin)
+    @pin = current_user.pins.build
   end
 
   def edit
@@ -22,7 +26,7 @@ class PinsController < ApplicationController
 
 # This is actually using the new action + create action
   def create
-    @pin = Pin.new(pin_params)
+    @pin = current_user.pins.build(pin_params)
     if @pin.save
       redirect_to @pin, notice: 'Pin was successfully created!'
     else
@@ -46,6 +50,11 @@ class PinsController < ApplicationController
   private
     def set_pin
       @pin = Pin.find(params[:id])
+    end
+
+    def correct_user
+      @pin = current_user.pins.find_by(id: params[:id])
+      redirect_to pins_path, notice: "Not authorized to edit this pin." if @pin.nil?
     end
 
     def pin_params
--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,2 +1,5 @@
 class Pin < ActiveRecord::Base
+	# use singular user. one pin belongs to a user
+	# Pin now has a user method.
+	belongs_to :user
 end
